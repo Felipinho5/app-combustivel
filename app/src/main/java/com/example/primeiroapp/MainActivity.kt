dc76705aaadbe15fd958c2cca5ca72f92f00bc76
Felipe de Carvalho Santos <142360153+Felipinho5@users.noreply.github.com>
2025-05-13 08:10:29 -0300

    Initial commit

---
+++ app/src/main/java/com/example/primeiroapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.primeiroapp
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.primeiroapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.gravar.setOnClickListener(this)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.gravar) {
+            binding.resposta.text = "Foi clicado"
+        }
+    }
+}
